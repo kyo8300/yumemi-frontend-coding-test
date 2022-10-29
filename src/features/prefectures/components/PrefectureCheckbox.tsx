da8a98742c1dbf5bdfe023992611b3e84da3096a
kyo8300 <kametyou777@gmail.com>
2022-10-29 20:39:03 +0800

    :lipstick: update css

--- src/features/prefectures/components/PrefectureCheckbox.tsx
+++ src/features/prefectures/components/PrefectureCheckbox.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./PrefectureCheckbox.css";
 import { Prefectures } from "../types";
 
 export const PrefectureCheckbox = ({ prefName, prefCode }: Prefectures) => {
   return (
-    <label>
+    <label className="prefectures-checkbox">
       <input type="checkbox" id={`${prefCode}`} name={`${prefName}`} />
       <span>{prefName}</span>
     </label>
